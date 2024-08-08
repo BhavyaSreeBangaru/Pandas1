7046364ca3b6a07f8f66596246dbd3da1adc6352
BhavyaSreeBangaru <bangaru.bhavyasree@diggibyte.com>
2024-08-08 19:13:09 +0530

    3rd

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import pandas as pd
-df=pd.DataFrame({"name":[2]})
+import numpy as np
+from utils import *
+data={'Name':['Bhavya','Dhanu','Sindhu'],
+      'Age':[22,19,20]}
+df=pd.DataFrame(data)
+print(df)
+df=add_column(df,'Dept',['HR','MR','HR'])
+df=add_column(df,'Review',[50,70,30])
+print(df)
+df=remove_column(df,'Age')
+print(df)
+df=filter_rows(df, 'Dept == "HR"')
+print(df)
+df=rename_columns(df, {'Name':'Names',
+                       'Dept':'Departments'})
+print(df)
 print(df.head())
+df=sum_column(df,'Review')
+print(df)
--- utils.py
+++ utils.py
@@ -1,2 +1,20 @@
-git config --global user.email "bangaru.bhavyasree@diggibyte.com"
-git config --global user.name "BhavyaSreebangaru"
+#git config --global user.email "bangaru.bhavyasree@diggibyte.com"
+#git config --global user.name "BhavyaSreeBangaru"
+import pandas as pd
+def add_column(df: pd.DataFrame, column_name: str, data: list) -> pd.DataFrame:
+    df[column_name] = data
+    return df
+def remove_column(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    df = df.drop(columns=[column_name])
+    return df
+def filter_rows(df: pd.DataFrame, condition: str) -> pd.DataFrame:
+    df = df.query(condition)
+    return df
+def rename_columns(df: pd.DataFrame, columns_dict: dict) -> pd.DataFrame:
+    df = df.rename(columns=columns_dict)
+    return df
+def sum_column(df: pd.DataFrame, column_name: str) -> float:
+    return df[column_name].sum()
+
+def filter_by_col(df, col_name, val):
+    return df[df[col_name] == val]
